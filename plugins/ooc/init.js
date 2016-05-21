390ad3f15fd60c9cdb8f9de25f865328236225db
Trinitro21 <trinitro21@yahoo.com>
2016-05-21 17:53:30 -0400

    just a little more efficient
    why does chat use <br></br>

--- plugins/ooc/init.js
+++ plugins/ooc/init.js
@@ -64,9 +64,8 @@ setTimeout(function(){
    var n=mess.querySelector("figcaption").innerHTML+mess.querySelector("user-rank").innerHTML;
    var c="";
    [].slice.call(mess.querySelectorAll("p")).forEach(function(i){
-    c+="\n"+i.innerHTML.replace(/<br>/g,"\n").replace(/<\/?a[^>]*>/g,"");
+    c+=(c.length>0?"\n":"")+i.innerHTML.replace(/<br(></br)?>/g,"\n").replace(/<\/?a[^>]*>/g,"");
    });
-   c=c.substring(1,c.length);
    updatejs(n,c);
   };
  }
